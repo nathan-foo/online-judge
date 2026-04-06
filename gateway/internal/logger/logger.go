5c53f2be70d618080fc7730bbe2ea20aabb409cc
Nathan Foo <ntfoo27@outlook.com>
2026-04-06 18:13:04 -0500

    Async/buffered logging

--- gateway/internal/logger/logger.go
+++ gateway/internal/logger/logger.go
@@ -1,18 +1,25 @@
 package logger
 
 import (
+	"fmt"
 	"net/http"
 	"os"
 	"time"
 
 	"github.com/go-chi/chi/v5/middleware"
 	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/diode"
 	"github.com/rs/zerolog/log"
 )
 
 func Init() {
 	zerolog.DurationFieldInteger = true
-	log.Logger = zerolog.New(os.Stderr).With().Timestamp().Logger()
+
+	w := diode.NewWriter(os.Stderr, 1000, 10*time.Millisecond, func(missed int) {
+		fmt.Fprintf(os.Stderr, "logger dropped %d messages\n", missed)
+	})
+
+	log.Logger = zerolog.New(w).With().Timestamp().Logger()
 }
 
 func RequestLogger(next http.Handler) http.Handler {
