6bd4824b970983953166e46fc405372b2b24d2a5
Nathan Foo <ntfoo27@outlook.com>
2026-04-29 14:16:18 -0500

    Create user service

--- gateway/internal/config/routes.go
+++ gateway/internal/config/routes.go
@@ -2,6 +2,13 @@ package config
 
 var Routes = []RouteConfig{
 	{
+		Prefix:        "/users",
+		ServiceUrl:    getConfig("USER_SERVICE_URL"),
+		RequireAuth:   true,
+		RateLimit:     10,
+		MaxUploadSize: MaxUploadSize,
+	},
+	{
 		Prefix:        "/test",
 		ServiceUrl:    getConfig("TEST_SERVICE_URL"),
 		RequireAuth:   true,
